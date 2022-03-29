77459bddce9d6b23c4216f913a83ef313e79e821
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-03-29 09:02:14 -0400

    useRef example2 (getting previous State of an element)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import UseRefExample1 from "./components/UseRefExample";
+import UseRefExample2 from "./components/UseRefExample2";
 
 function App() {
   return (
     <div className="container mt-5">
       <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 }
---
+++ src/components/UseRefExample2.js
@@ -0,0 +1,27 @@
+import { useState, useRef, useEffect } from "react";
+
+function useRefExample2() {
+  const renders = useRef(1);
+  const prevName = useRef("");
+  const [name, setname] = useState("");
+
+  useEffect(() => {
+    renders.current = renders.current + 1;
+    prevName.current = name;
+  }, [name]);
+  return (
+    <div>
+      <h3>Example 2. useRef to get previous state of a component.</h3>
+      <p>Renders: {renders.current}</p>
+      <p>Previous name state: {prevName.current}</p>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+        className="form-control mb-3x"
+      />
+    </div>
+  );
+}
+
+export default useRefExample2;
